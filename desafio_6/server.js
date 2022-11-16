4902624147fd5c3a34c0cec31d8f6d9463ad7127
Gary Calle <garyssteven11@gmail.com>
2022-11-16 14:57:57 -0300

    add babel

--- desafio_6/server.js
+++ desafio_6/server.js
@@ -1,27 +1,20 @@
-import express from 'express'
 import multer from 'multer'
-import Contenedor from './src/Contenedor.js'
 import productRoutes from './src/routes/productos.routes.js'
-import { server as webSocket } from 'socket.io'
+import { Server as webSocket } from 'socket.io'
 import http from 'http'
+import app from './app.js'
 
 // ? INICIO DE SERVIDOR PARA SOCKETS
-const app = express()
-// ------
+
 const httpServer = http.createServer(app)
 const io = new webSocket(httpServer)
 const PORT = 8080
 httpServer.listen(PORT)
-console.log(`Servidor en puerto http://localhost:${PORT}`);
+console.log(`Servidor en puerto http://localhost:${PORT}`)
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static('public'))
-app.set('views', './src/views')
-app.set('view engine', 'ejs')
-const { pathname: root } = new URL('../src/views', import.meta.url)
 
 // ** multer
+
 const storage = multer.diskStorage({
   destination: './src/public/files',
   filename: (req, file, cb) => {
@@ -35,19 +28,21 @@ app.use(
   }).single('imagen')
 )
 
-app.get('/', (req, res) => {
-  res.render('productos.ejs')
-})
+// app.get('/', (req, res) => {
+//   res.render('index.html')
+// })
 app.use('/api/productos', productRoutes)
 
 //** Inicio de sockets */
 const mensaje = []
 
-io.on('connnection', (socket) => {
-  console.log(`Nueva conexion cliente ${socket.id}`);
+io.on('connection', (socket) => {
+  console.log(`Nueva conexion cliente ${socket.id}`)
+  socket.emit('hello', 'world')
   socket.on('client:chat', (data) => {
     const chat = { ...data }
-    mensajes.push(chat)
+    console.log(data)
+    mensaje.push(chat)
     io.sockets.emit('server:chat', chat)
   })
 })
--- desafio_6/src/public/main.js
+++ desafio_6/src/public/main.js
@@ -1,8 +1,10 @@
 
-
+const socket = io()
+socket.on('hello', (arg) => {
+  console.log(arg) // world
+})
 const render = (data) => {
-    notes.innerHTML +=
-        `
+  notes.innerHTML += `
     <div class=" card card-body rounded-0 mb-2 animate__bounceIn">
         <div class="list-group chat">
             <div class="d-flex w-100 justify-content-between">
@@ -17,35 +19,34 @@ const render = (data) => {
 }
 // Agregar mensajes que envian desde el form en plantilla
 
-const addMessage = e => {
-    const message = {
-        user: document.getElementById('user').value,
-        text: document.getElementById('text').value,
-        date: dateNow()
-    }
-    socket.emit('client:chat', message)
-    return false
+const addMessage = (e) => {
+  const message = {
+    user: document.getElementById('user').value,
+    text: document.getElementById('text').value,
+    date: dateNow(),
+  }
+  socket.emit('client:chat', message)
+  return false
 }
 
 // fecha y hora del mensaje
 
 dateNow = () => {
-    const now = new Date()
-    return `${now.getHours()}: ${now.getMinutes()}`
-
+  const now = new Date()
+  return `${now.getHours()}: ${now.getMinutes()}`
 }
 
 const cargar = (nota) => {
-    nota.forEach((nota) => render.nota)
+  nota.forEach((nota) => render.nota)
 }
 
 // card productos
 
 const displayProductos = (productList) => {
-    let productosHTML = ''
+  let productosHTML = ''
 
-    productList.forEach((element) => {
-        productosHTML += `<div class="col text-center mt-4 text-capitalize" >
+  productList.forEach((element) => {
+    productosHTML += `<div class="col text-center mt-4 text-capitalize" >
            
         <div class="card h-100 card_img shadow_sm">
             <img src="${element.thumbnail}" class="card-img-top" alt="${element.title}">
@@ -56,8 +57,8 @@ const displayProductos = (productList) => {
                 <button class="btn btn-primary mt-2 " onclick="add(${element.id}, ${element.price})">Comprar</button>
             </div>
         </div>
-        </div>`;
-    })
+        </div>`
+  })
 }
 
 document.getElementById('insert_product').innerHTML = productosHTML
