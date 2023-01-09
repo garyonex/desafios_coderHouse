99178dc8daabd11d1e9b3a1619c10d5295f57db1
Gary Calle <94655079+garyonex@users.noreply.github.com>
2023-01-09 16:22:53 -0300

    refactor( api ) routes

--- entrega_dos_Nuevo/api/src/routes/cart.routes.js
+++ entrega_dos_Nuevo/api/src/routes/cart.routes.js
@@ -5,10 +5,11 @@ import {
   addProductCart,
   getProductsCart
 } from '../controllers/cart.controllers'
+import { authToken } from '../middleware/authToken'
 
 const cartRoutes = Router()
-cartRoutes.get('/', getProductsCart)
-cartRoutes.post('/', addProductCart)
-cartRoutes.put('/:productId ', updateProductCart)
-cartRoutes.delete('/', deleteProductCart)
+cartRoutes.get('/', authToken, getProductsCart)
+cartRoutes.post('/', authToken, addProductCart)
+cartRoutes.put('/:productId ', authToken, updateProductCart)
+cartRoutes.delete('/', authToken, deleteProductCart)
 export default cartRoutes
--- entrega_dos_Nuevo/app/src/Components/Products/index.jsx
+++ entrega_dos_Nuevo/app/src/Components/Products/index.jsx
@@ -3,12 +3,15 @@ import CartContext from '../../Context/CartContext'
 import Loading from '../../helpers/Loading'
 import { useLoading } from '../../hooks/useLoading'
 import { useProducts } from '../../hooks/useProducts'
+import { useUser } from '../../hooks/useUser'
 import Item from '../Item'
 import './styles.modules.scss'
 const Products = () => {
   const { addItem } = useContext(CartContext)
   const { products } = useProducts()
   const { loading } = useLoading()
+  const { user } = useUser()
+  const token = user.token
   return (
     <div className='productsTotals'>
       {loading ? <Loading /> : ''}
@@ -18,7 +21,7 @@ const Products = () => {
             <Item key={item._id} {...item} />
             {!item.inCart ? (
               <div className='card-btn'>
-                <button onClick={() => addItem(item)}> Add to Cart</button>
+                <button onClick={() => addItem(item, token)}> Add to Cart</button>
               </div>
             ) : (
               <button>EN EL CARRITO</button>
